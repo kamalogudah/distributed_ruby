42474d5f446713760fe3971ba54e3a5607bf743f
kamalogudah <mcpaul2058@gmail.com>
2018-12-13 10:31:47 +0300

    Added Security to the Server

--- hello_distributed.rb
+++ hello_distributed.rb
@@ -1,10 +1,16 @@
 require 'drb'
+require 'drb/acl'
+
+# make sure no one can call eval() and related methods remotely!
+$SAFE = 1
 
 class HelloWorldServer
   def say_hello
     'Hello, World!'
   end
 end
+
+acl = ACL.NEW(%w{ deny all allow 192.168.0.13 })
 
 DRb.start_service("druby://127.0.0.1:61676", HelloWorldServer.new)
 DRb.thread.join
