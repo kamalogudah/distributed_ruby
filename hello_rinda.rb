56bd64ad08e5c44fba9aa3a263765bdec45a5bec
kamalogudah <mcpaul2058@gmail.com>
2019-02-20 10:37:47 +0300

    Using Hashes for Tuples

--- hello_rinda.rb
+++ hello_rinda.rb
@@ -13,6 +13,8 @@ DRb.start_service
 Rinda::RingServer.new(Rinda::TupleSpace.new, 8080)
 Rinda::RingServer.new(Rinda::TupleSpace.new)
 ring_server = Rinda::RingFinger.primary
-ring_server.write([:hello_world_service, :HelloWorldServer, HelloWorldServer.new,
-'I like to say hi!'], Rinda::SimpleRenewer.new )
+# ring_server.write([:hello_world_service, :HelloWorldServer, HelloWorldServer.new,
+# 'I like to say hi!'], Rinda::SimpleRenewer.new )
+ring_server.write({'service_name' => :hello_world_service, 'service_class' => :HelloWorldServer,'service_instance' => HelloWorldServer.new,
+'service_description' => 'I like to say hi!'}, Rinda::SimpleRenewer.new )
 DRb.thread.join
--- hello_rinda_client.rb
+++ hello_rinda_client.rb
@@ -1,8 +1,10 @@
 require 'rinda/ring'
 DRb.start_service
 ring_server = Rinda::RingFinger.primary
-service = ring_server.read([:hello_world_service, nil, nil, nil])
-server = service[2]
+# service = ring_server.read([:hello_world_service, nil, nil, nil])
+service = ring_server.read({'service_name' => :hello_world_service, 'service_class' => nil, 'service_instance' => nil, 'service_description' => nil})
+# server = service[2]
+server = service['service_instance']
 
 puts server.say_hello
 puts service.inspect
